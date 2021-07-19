78328b9d9a888a9812fde236c24e71bd1b9ef9b4
Alex <chel213@ya.ru>
2021-07-20 00:27:00 +0300

    feat: add pug templates

--- src/express/express.js
+++ src/express/express.js
@@ -7,6 +7,11 @@ const articlesRouter = require(`./routes/articles`);
 const app = express();
 const DEFAULT_PORT = 8080;
 
+app.set(`views`, `${__dirname}/templates`);
+app.set(`view engine`, `pug`);
+
+app.use(express.static(`${__dirname}/public`));
+
 app.use(`/my`, myRouter);
 app.use(`/articles`, articlesRouter);
 
@@ -16,21 +21,21 @@ app.listen(DEFAULT_PORT, ()=>{
 });
 
 app.get(`/`, (req, res)=>{
-  res.send(`/`);
+  res.render(`main`);
 });
 
 app.get(`/register`, (req, res)=>{
-  res.send(`/register`);
+  res.render(`auth/sign-up`);
 });
 
 app.get(`/login`, (req, res)=>{
-  res.send(`/login`);
+  res.render(`auth/sign-in`);
 });
 
 app.get(`/search`, (req, res)=>{
-  res.send(`/search`);
+  res.render(`search/search`);
 });
 
 app.get(`/categories`, (req, res)=>{
-  res.send(`/categories`);
+  res.render(`categories/all-categories`);
 });
